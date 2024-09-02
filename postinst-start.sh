39e6b51ea5bd794e76bc6348b5a03195dc2337ac
Andrew Hall <ahall@redquasar.net>
2024-09-02 23:54:46 +1000

    more bugfixes

--- postinst-start.sh
+++ postinst-start.sh
@@ -2,7 +2,7 @@
 
 source "postinst-yad.sh"
 
-trap yad_message_ok "Setup Error" "An error has caused setup to fail and automatic deployment to stop.\nYour device will be restarted and setup will try again." "dialog-error" EXIT
+trap "yad_message_ok 'Setup Error' 'An error has caused setup to fail and automatic deployment to stop.\nYour device will be restarted and setup will try again.' 'dialog-error'" EXIT
 
 function exit_on_error() {
   if [ $? -ne 0 ]; then
