f4214384787e8f1fcec236c57a22c024bccf025b
Andrew Hall <ahall@redquasar.net>
2024-09-03 11:56:19 +1000

    improvements

--- postinst-yad.sh
+++ postinst-yad.sh
@@ -8,13 +8,13 @@ export YAD_NOCLOSE="--no-escape --no-buttons"
 export YAD_PARAMS="--on-top --center"
 
 function yad_progress() {
-    yad --progress --title="$1" --text="$2" --image="$3" --percentage=0 ${YAD_AUTOKILLCLOSE} $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
+    yad --progress --title="$1" --text="$2" --image="$3" --percentage=0 $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
 }
 
 function yad_progress_pulsate() {
-    yad --progress --title="$1" --text="$2" --image="$3" --pulsate ${YAD_AUTOKILLCLOSE} $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
+    yad --progress --title="$1" --text="$2" --image="$3" --pulsate $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
 }
 
 function yad_message_ok() {
-    yad --image "$3" --title "$1" --button=Ok:0 --text "$2" $YAD_PARAMS $YAD_NOCLOSE --width="$YAD_WIDTH" 
+    yad --image "$3" --title "$1" --button=Ok:0 --text "$2" $YAD_PARAMS --width="$YAD_WIDTH" 
 }
