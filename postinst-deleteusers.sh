7ad665e66efb8a34eb8e87152d8ac5bc74421e47
Andrew Hall <ahall@redquasar.net>
2024-09-03 11:49:47 +1000

    improvements

--- postinst-deleteusers.sh
+++ postinst-deleteusers.sh
@@ -10,8 +10,9 @@ for user in $(awk -F: '$3 >= 1000 {print $1}' /etc/passwd); do
     if [ "$user" != "nobody" ]; then
         #mv /home/$user /home/${user}-${BACKUP_DATE}-presetup
         #userdel -r $user
-        sleep 1
-        echo "simuating user deletion for early debugging purposes."
     fi
 done
+# We don't want to delete anything yet as it'll make early debugging a pain, so we'll just run a loop that only updates the gui.
+echo "simuating user deletion for early debugging purposes."
+for (( i=0 ; i < 100 ; i++ )) ; do echo $i; sleep 0.05 ; done
 ) | yad_progress_pulsate "Post Install" "Deleting existing user data." "clock"
