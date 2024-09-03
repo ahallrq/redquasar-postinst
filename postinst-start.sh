bc18a333fee09f9a0c32f33b6ccfad0c9c36fd40
Andrew Hall <ahall@redquasar.net>
2024-09-03 12:09:10 +1000

    add pkg installation

--- postinst-start.sh
+++ postinst-start.sh
@@ -17,3 +17,4 @@ echo "100";
 
 ./postinst-resetnetworking.sh || exit_on_error "Failed to reset networking."
 ./postinst-deleteusers.sh || exit_on_error "Failed to reset users."
+./postinst-installpackages.sh || exit_on_error "Failed to install packages."
--- postinst-yad.sh
+++ postinst-yad.sh
@@ -6,13 +6,22 @@ export YAD_WIDTH=600
 export YAD_AUTOKILLCLOSE="--auto-close --auto-kill"
 export YAD_NOCLOSE="--no-escape --no-buttons"
 export YAD_PARAMS="--on-top --center"
+export YAD_PROGLOG="--enable-log --log-expanded"
 
 function yad_progress() {
+    yad --progress --title="$1" --text="$2" --image="$3" --percentage=0 $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE $YAD_PROGLOG --width=$YAD_WIDTH
+}
+
+function yad_progress_log() {
     yad --progress --title="$1" --text="$2" --image="$3" --percentage=0 $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
 }
 
 function yad_progress_pulsate() {
     yad --progress --title="$1" --text="$2" --image="$3" --pulsate $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE --width=$YAD_WIDTH
+}
+
+function yad_progress_pulsate_log() {
+    yad --progress --title="$1" --text="$2" --image="$3" --pulsate $YAD_AUTOKILLCLOSE $YAD_PARAMS $YAD_NOCLOSE $YAD_PROGLOG --width=$YAD_WIDTH
 }
 
 function yad_message_ok() {
